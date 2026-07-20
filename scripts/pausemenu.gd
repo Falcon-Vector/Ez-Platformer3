3883f2d8226007b7e3b8ad0984bce1df218423f0
Falcon-Vector <falconx31.777@gmail.com>
2026-07-21 02:47:01 +0530

    added pause menu, updated tile map

---
+++ scripts/pausemenu.gd
@@ -0,0 +1,35 @@
+extends Control
+
+func _ready() -> void:
+	hide()
+
+func resume():
+	hide()
+	get_tree().paused = false
+	
+func pause():
+	show()
+	get_tree().paused = true
+	
+func testEsc():
+	if Input.is_action_just_pressed("esc") and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused == true:
+		resume()
+
+
+func _on_resume_pressed() -> void:
+	resume()
+
+
+func _on_restart_pressed() -> void:
+	resume()
+	get_tree().reload_current_scene()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _process(delta):
+	testEsc()
